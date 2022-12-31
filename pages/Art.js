d748c57454bfffed716547bc94f721e853ef1b6b
mel <mellee115@hotmail.com>
2022-12-31 14:53:05 -0500

    Revert "Moving images folder to public folder"
    This reverts commit 37445a00ef168645d368dc71ef760c83b6764b82.

--- pages/Art.js
+++ pages/Art.js
@@ -1,20 +1,20 @@
 import React from "react";
 import {ImgGrid} from "../components/ImgGrid";
 
-import img1 from "../public/images/art/blue gouache rose 1.png"
-import img2 from "../public/images/art/mushrooms stamp.jpg"
-import img3 from "../public/images/art/mushrooms stamp print.jpg"
-import img4 from "../public/images/art/abstract acrylic painting 2.jpg"
-import img5 from "../public/images/art/bun coloured w mint green.png"
-import img6 from "../public/images/art/tulips with blue grid.jpg"
-import img7 from "../public/images/art/gouache pear.jpg"
-import img8 from "../public/images/art/polymer clay crane.jpg"
-import img9 from "../public/images/art/abstract acrylic painting 1.jpg"
-import img10 from "../public/images/art/Red Gouache Rose.jpg"
-import img11 from "../public/images/art/Monstera.jpg"
-import img12 from "../public/images/art/cave mushrooms.jpg"
-import img13 from "../public/images/art/greyhound bow.jpg"
-import img14 from "../public/images/art/bonsai trees.jpg"
+import img1 from "../assets/images/art/blue gouache rose 1.png"
+import img2 from "../assets/images/art/mushrooms stamp.jpg"
+import img3 from "../assets/images/art/mushrooms stamp print.jpg"
+import img4 from "../assets/images/art/abstract acrylic painting 2.jpg"
+import img5 from "../assets/images/art/bun coloured w mint green.png"
+import img6 from "../assets/images/art/tulips with blue grid.jpg"
+import img7 from "../assets/images/art/gouache pear.jpg"
+import img8 from "../assets/images/art/polymer clay crane.jpg"
+import img9 from "../assets/images/art/abstract acrylic painting 1.jpg"
+import img10 from "../assets/images/art/Red Gouache Rose.jpg"
+import img11 from "../assets/images/art/Monstera.jpg"
+import img12 from "../assets/images/art/cave mushrooms.jpg"
+import img13 from "../assets/images/art/greyhound bow.jpg"
+import img14 from "../assets/images/art/bonsai trees.jpg"
 
 export const Art = () => {
   //TODO: rewrite this in a better way?
