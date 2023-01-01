e8c13f48c4acfd9de4b19ceace37f3e0589e4c01
mel <mellee115@hotmail.com>
2023-01-01 00:38:31 -0500

    Removing imports of image to see if deployment error resolves

--- pages/Art.js
+++ pages/Art.js
@@ -1,38 +1,23 @@
 import React from "react";
 import {ImgGrid} from "../components/ImgGrid";
 
-import img1 from "../public/images/art/blue gouache rose 1.png"
-import img2 from "../public/images/art/mushrooms stamp.jpg"
-import img3 from "../public/images/art/mushrooms stamp print.jpg"
-import img4 from "../public/images/art/abstract acrylic painting 2.jpg"
-import img5 from "../public/images/art/bun coloured w mint green.png"
-import img6 from "../public/images/art/tulips with blue grid.jpg"
-import img7 from "../public/images/art/gouache pear.jpg"
-import img8 from "../public/images/art/polymer clay crane.jpg"
-import img9 from "../public/images/art/abstract acrylic painting 1.jpg"
-import img10 from "../public/images/art/Red Gouache Rose.jpg"
-import img11 from "../public/images/art/Monstera.jpg"
-import img12 from "../public/images/art/cave mushrooms.jpg"
-import img13 from "../public/images/art/greyhound bow.jpg"
-import img14 from "../public/images/art/bonsai trees.jpg"
-
 export const Art = () => {
   //TODO: rewrite this in a better way?
   let imgs = [
-    {src:img1},
-    {src:img2},
-    {src:img3},
-    {src:img4},
-    {src:img5},
-    {src:img6},
-    {src:img7},
-    {src:img8},
-    {src:img9},
-    {src:img10},
-    {src:img11},
-    {src:img12},
-    {src:img13},
-    {src:img14},
+    {src:"/images/art/blue gouache rose 1.png"},
+    {src:"/images/art/mushrooms stamp.jpg"},
+    {src:"/images/art/mushrooms stamp print.jpg"},
+    {src:"/images/art/abstract acrylic painting 2.jpg"},
+    {src:"/images/art/bun coloured w mint green.png"},
+    {src:"/images/art/tulips with blue grid.jpg"},
+    {src:"/images/art/gouache pear.jpg"},
+    {src:"/images/art/polymer clay crane.jpg"},
+    {src:"/images/art/abstract acrylic painting 1.jpg"},
+    {src:"/images/art/Red Gouache Rose.jpg"},
+    {src:"/images/art/Monstera.jpg"},
+    {src:"/images/art/cave mushrooms.jpg"},
+    {src:"/images/art/greyhound bow.jpg"},
+    {src:"/images/art/bonsai trees.jpg"},
   ]
 
   return(<>
--- pages/Photos.js
+++ pages/Photos.js
@@ -1,38 +1,24 @@
 import React from "react";
 import {ImgGrid} from "../components/ImgGrid";
 
-import img1 from  "../public/images/photos/Toronto Sakura.JPG"
-import img2 from  "../public/images/photos/Sweet Dreams Noodle Soup Special.JPG"
-import img3 from  "../public/images/photos/Sweet Dreams Noodle Soup Smile.JPG"
-import img4 from  "../public/images/photos/Sweet Dreams Mango Bingsu Special.JPG"
-import img5 from  "../public/images/photos/Sweet Dreams Overhead Noodle Soup Special.JPG"
-import img6 from  "../public/images/photos/Macarons.JPG"
-import img7 from  "../public/images/photos/Sweet Dreams Hot Drink Special.JPG"
-import img8 from  "../public/images/photos/Sweet Dreams at night.JPG"
-import img9 from  "../public/images/photos/Rock Dunder Tiny Mushrooms.JPG"
-import img10 from "../public/images/photos/Rock Dunder Tree Mushrooms.JPG"
-import img11 from "../public/images/photos/Rock Dunder Juniper.JPG"
-import img12 from "../public/images/photos/Super Flower Blood Moon.JPG"
-import img13 from "../public/images/photos/Morning Moon.JPG"
-import img14 from "../public/images/photos/Eucalyptus.JPG"
 
 export const Photos = () => {
   //TODO: rewrite this in a better way?
   let imgs = [
-    {src: img1},
-    {src: img2},
-    {src: img3},
-    {src: img4},
-    {src: img5},
-    {src: img6},
-    {src: img7},
-    {src: img8},
-    {src: img9},
-    {src: img10},
-    {src: img11},
-    {src: img12},
-    {src: img13},
-    {src: img14},
+    {src: "/images/photos/Toronto Sakura.JPG"},
+    {src: "/images/photos/Sweet Dreams Noodle Soup Special.JPG"},
+    {src: "/images/photos/Sweet Dreams Noodle Soup Smile.JPG"},
+    {src: "/images/photos/Sweet Dreams Mango Bingsu Special.JPG"},
+    {src: "/images/photos/Sweet Dreams Overhead Noodle Soup Special.JPG"},
+    {src: "/images/photos/Macarons.JPG"},
+    {src: "/images/photos/Sweet Dreams Hot Drink Special.JPG"},
+    {src: "/images/photos/Sweet Dreams at night.JPG"},
+    {src: "/images/photos/Rock Dunder Tiny Mushrooms.JPG"},
+    {src: "/images/photos/Rock Dunder Tree Mushrooms.JPG"},
+    {src: "/images/photos/Rock Dunder Juniper.JPG"},
+    {src: "/images/photos/Super Flower Blood Moon.JPG"},
+    {src: "/images/photos/Morning Moon.JPG"},
+    {src: "/images/photos/Eucalyptus.JPG"},
   ]
 
   return (<>
